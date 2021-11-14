dfe5ef5948decbed199388747159081fc7e7f45d
mediocreatmybest <80406625+mediocreatmybest@users.noreply.github.com>
2021-11-14 19:08:47 +0930

    Quick way to use Video2X container with corrected repository and some extra apps

---
+++ Video2x-app/example-start.sh
@@ -0,0 +1,1 @@
+docker run --rm -v /mnt/video_data/:/video_data -it --gpus all -v /dev/dri/:/dev/dri --privileged video2x-app bash
---
+++ Video2x-app/readme.md
@@ -0,0 +1,1 @@
+Quick and easy way to get the Video2X docker running with a corrected repository and some extra applications.
