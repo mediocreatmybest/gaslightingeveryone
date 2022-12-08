99afe7c92372ff88d557a80eb93978a2828dd402
mediocreatmybest <80406625+mediocreatmybest@users.noreply.github.com>
2022-12-08 15:05:49 +0930

    config test

--- Scripts/kickstart-dl.py
+++ Scripts/kickstart-dl.py
@@ -2,149 +2,182 @@ import subprocess
 import csv
 import urllib
 import urllib.request
+from configparser import ConfigParser
 import argparse
+import pathlib
 from pathlib import Path
 
 # This script is intended to be used with plain text only
 
 # List of supported sites.
 SUPPORTED_FILTER_URLS = ['www.reddit.com','www.unsplash.com', 'www.artstation.com']
-# URL filters for supported websites (reddit, etc.) This will allow us to loop through each type of top search
-REDDIT_TOP = ['/top/?t=all','/top/?t=month']
-UNSPLASH_SEARCH_FILTER = ['/cats?orientation=squarish']
-ARTSTATION_SEARCH_FILTER = ['?sort_by=rank']
 
 # Gallery-DL defaults
 GALLER_CMD ='gallery-dl'
 GALLERY_ARG = '--write-metadata --sleep 2-4 --range 1-1'
 
-
 # YT-DLP Defaults ######## To do ######
 YTDLP_CMD = 'yt-dlp'
 YTDLP_ARG = '--write-info-json'
 
+config_parser = ConfigParser()
+
+# Get the scripts parent directory so we can locate the config file
+
+workingdir = Path( __file__ ).parent.absolute()
+# print('This is the script dir:', workingdir)
+
+# Name of working config file
+# *Todo: * Switch this to a list, just because
+configfile = 'config.ini'
+
+# Join the working parent directory with the config file
+workingconfig = pathlib.Path.joinpath(workingdir, configfile)
+
+# Set how to read the contents of the config
+read_files = config_parser.read(workingconfig)
 
 # Create the parser
 parser = argparse.ArgumentParser()
-# Add an argument
+# Command line arguments will overrule the config file (ideally)
 parser.add_argument('--downloader', type=str, choices=['gallery-dl', 'yt-dlp'], required=False)
 parser.add_argument('--type', type=str, choices=['url', 'file'], required=False)
-parser.add_argument('--txtmode', type=str, choices=['csv', 'plain'], required=False)
-parser.add_argument('--sourcelist', type=str, required=True)
-parser.add_argument('--directory', type=str, required=True)
+parser.add_argument('--mode', type=str, choices=['csv', 'txt'], required=False)
+parser.add_argument('--sourcelist', type=str, required=False)
+parser.add_argument('--directory', type=str, required=False)
 
 
 # Parse the argument
 CMD_ARGS = parser.parse_args()
 
 # Set Defaults if non selected
-if CMD_ARGS.downloader == None:
+if CMD_ARGS.downloader is None:
     CMD_ARGS.downloader = 'gallery-dl'
-if CMD_ARGS.type == None:
-    CMD_ARGS.type = 'url'
-if CMD_ARGS.txtmode == None:
-    CMD_ARGS.txtmode = 'plain'
+if CMD_ARGS.type is None:
+    SRC_LIST_TYPE = config_parser.get('src_list', 'type')
+else:
+    SRC_LIST_TYPE = CMD_ARGS.type
+
+if CMD_ARGS.mode is None:
+    GLOBAL_MODE = config_parser.get('global', 'mode')
+else:
+    GLOBAL_MODE = CMD_ARGS.mode
+if CMD_ARGS.sourcelist is None:
+    TXT_SRC = config_parser.get('src_list', 'txt_src')
+else:
+    TXT_SRC = CMD_ARGS.sourcelist
+
+# Set directory location based off command line arguments or config file
+if CMD_ARGS.directory is None:
+    DIRLOC = config_parser.get('global', 'directory')
+    GALLERY_EXTRACT_PATH = f'--directory {DIRLOC}'
+    YTDLP_DL_PATH = f'--output {DIRLOC}'
+else:
+    GALLERY_EXTRACT_PATH = f'--directory {CMD_ARGS.directory}'
+    YTDLP_DL_PATH = f'--output {CMD_ARGS.directory}'
 
 print('program selected is:', CMD_ARGS.downloader)
-print('program type is:', CMD_ARGS.type)
-print('program mode is:', CMD_ARGS.txtmode)
-print('program source list is:', CMD_ARGS.sourcelist)
+print('source list type is:', SRC_LIST_TYPE)
+print('The global mode is:', GLOBAL_MODE)
+print('program source list is:', TXT_SRC)
 
+# URL filters for supported websites (reddit, etc.) This will allow us to loop through each type of top search
+REDDIT_TOP = config_parser.get('web_config_reddit', 'filter')
+UNSPLASH_SEARCH_FILTER = config_parser.get('web_config_unsplash', 'filter')
+ARTSTATION_SEARCH_FILTER = config_parser.get('web_config_unsplash', 'filter')
 
-# Set this based on command line arguments or defaults
-if CMD_ARGS.txtmode == 'plain':
-    CSV_MODE = False
-else:
-    CSV_MODE = True
-
-#Set this based on command line arguments or defaults
-if CMD_ARGS.type == 'url':
-    FILE_MODE = False
-else:
-    FILE_MODE = True
-
-#Set this based on command line arguments
-#Currently file source is broken, only a url with raw text will work
-TXT_SRC = CMD_ARGS.sourcelist
-
-#Set directory location based off command line arguments
-GALLERY_EXTRACT_PATH = f'--directory {CMD_ARGS.directory}'
-YTDLP_DL_PATH = f'--output {CMD_ARGS.directory}'
-
-#Organise defaults into single string
+# Organise defaults into single string
 GALLERY_FULLCMDARG = GALLER_CMD.split() + GALLERY_EXTRACT_PATH.split() + GALLERY_ARG.split()
 
-#Doesn't really need to be a function but might as well see if this works.
-#Function to check if the data is parsed as a url, might not be the best way to do it.
+# Doesn't really need to be a function but might as well see if this works.
+# Function to check if the data is parsed as a url, might not be the best way to do it.
+
 def is_url_data_type():
-    #print('Checking if text source is URL')
-    function_url = urllib.parse.urlparse(TXT_SRC)
-    if function_url != '':
-        return True
+        print('Checking if text source is URL')
+        function_url = urllib.parse.urlparse(TXT_SRC)
+        # print(function_url.path)
+        # print(function_url)
+        if function_url.netloc == '':
+            print('netloc is empty')
+            print('path is:', function_url.path)
+            print('this does not pass the url sniff test')
+            return False
+        else:
+            print('netloc has data')
+            print('netloc is:', function_url.netloc)
+            return True
 
-#Parse URL details from https://www.simplified.guide/python/get-host-name-from-url
-#https://docs.python.org/3/library/urllib.parse.html#module-urllib.parse
 
-#Using plain text data as input data
-if CSV_MODE == False:
-    #Check if we are using a websites data
+# Parse URL details from https://www.simplified.guide/python/get-host-name-from-url
+# https://docs.python.org/3/library/urllib.parse.html#module-urllib.parse
+
+print('global is now:',  GLOBAL_MODE)
+print('SRC_LIST is now:',  SRC_LIST_TYPE)
+
+
+# Using plain text data as input data
+if GLOBAL_MODE == 'txt' and SRC_LIST_TYPE == 'url':
+    print('GLOBAL MODE IS TXT AND SRC LIST TYPE IS URL')
+    # Check if we are using a websites data
     if is_url_data_type() is True:
-        txt_url = TXT_SRC
-        with urllib.request.urlopen(txt_url) as txt_read:
-            #Read, decode and then split the final data from the webserver to allow reading each line
-            rawoutput = txt_read.read().decode('utf-8').split()
-            #Loop through each line
+        TXT_URL = TXT_SRC
+        print(TXT_URL)
+        with urllib.request.urlopen(TXT_URL) as TXT_READ:
+            # Read, decode and then split the final data from the webserver to allow reading each line
+            rawoutput = TXT_READ.read().decode('UTF-8').split()
+            # Loop through each line
             for eachdomain in rawoutput:
 
-                #Do a check against each domain as they may have different options
-                #Create URL Check Variable, not sure if this will work...
+                # Do a check against each domain as they may have different options
+                # Create URL Check Variable, not sure if this will work...
                 urlcheck = urllib.parse.urlparse(eachdomain).netloc
                 if urlcheck == 'www.reddit.com':
                     for topsearch in REDDIT_TOP:
                         EACHDOMAIN_TOP = eachdomain + topsearch
                         GALLERY_FULLCMDARG.append(EACHDOMAIN_TOP)
-                        result = subprocess.run(GALLERY_FULLCMDARG, capture_output=True, text=True, encoding='UTF-8')
+                        print('Reddit! Beep Boop!')
+                        #result = subprocess.run(GALLERY_FULLCMDARG, capture_output=True, text=True, encoding='UTF-8')
 
                 if urlcheck == 'www.unsplash.com':
                     GALLERY_FULLCMDARG.append(eachdomain)
-                    result = subprocess.run(GALLERY_FULLCMDARG, capture_output=True, text=True)
+                    print('Unsplash! Beep! Boop!')
+                    #result = subprocess.run(GALLERY_FULLCMDARG, capture_output=True, text=True)
 
                 if urlcheck == 'www.artstation.com':
                     GALLERY_FULLCMDARG.append(eachdomain)
-                    result = subprocess.run(GALLERY_FULLCMDARG, capture_output=True, text=True)
+                    print('Artstation! Beep Boop!')
+                    #result = subprocess.run(GALLERY_FULLCMDARG, capture_output=True, text=True)
 
-                #if (web1 != query and web2 != query and web3 != query):
-                #   print('This should only be triggered if Web1 and Web2 and Web3 are not found')
-
-                #Catch any websites that don't exist in the supported filter and do standard download
+                # Catch any websites that don't exist in the supported filter and do standard download
                 if (urlcheck != SUPPORTED_FILTER_URLS):
                     GALLERY_FULLCMDARG.append(eachdomain)
-                    result = subprocess.run(GALLERY_FULLCMDARG, capture_output=True, text=True)
+                    print('CATCHALL! Beep Boop!')
+                    #result = subprocess.run(GALLERY_FULLCMDARG, capture_output=True, text=True)
 
 
 
 
 
 
-    #Check if we are using a text file for data #Also this is broken.
-    if is_url_data_type() is False:
-        txt_file = Path(rf'{TXT_SRC}')  #Broken
-        print ('TXT MODE!')
-        print ('The test path is')
-        print(txt_file)
+    ##Check if we are using a text file for data #Also this is broken.
+    #if is_url_data_type() is False:
+    #    TXT_FILE = Path(rf'{TXT_SRC}')  #Broken
+    #    print ('TXT MODE!')
+    #    print ('The test path is')
+    #    print(TXT_FILE)
 
 
 #Using CSV seperated data instead of plain text.
-if CSV_MODE == True:
-    with open(txt_file) as csv_file:
-        csv_reader = csv.reader(csv_file, delimiter=',')
-        for row in csv_reader:
-            download = (row[0])
-            #domain_details = (row[1])
-            url = urllib.parse.urlparse(row[0])
-            print(url.netloc)
-            #print(is_valid_hostname(download))
-            print('CSV MODE!')
+#if GLOBAL_MODE == 'csv':
+#    with open(TXT_FILE) as csv_file:
+#        csv_reader = csv.reader(csv_file, delimiter=',')
+#        for row in csv_reader:
+#            download = (row[0])
+#            #domain_details = (row[1])
+#            url = urllib.parse.urlparse(row[0])
+#            print(url.netloc)
+#            #print(is_valid_hostname(download))
+#            print('CSV MODE!')
 
 
 
@@ -166,10 +199,10 @@ if CSV_MODE == True:
 #    print('###############################')
 #    print('\n')
 #
-print('###############################')
-print('errors:')
-print (result.stderr)
-print('###############################')
-print('Results:')
-print (result.stdout)
-print('###############################')
+#print('###############################')
+#print('errors:')
+#print (result.stderr)
+#print('###############################')
+#print('Results:')
+#print (result.stdout)
+#print('###############################')
--- Scripts/test.py
+++ Scripts/test.py
@@ -1,28 +1,40 @@
 
-from configparser import ConfigParser
 import glob
 import pathlib
+import os
+from configparser import ConfigParser
+from pathlib import Path
+
+
+
+
 
 curr_file = pathlib.Path(__file__)
-print(curr_file)
+#print(curr_file)
 
 
-config_parser = ConfigParser()
+#print('script dir',script_dir)
+
+
 # http://pymotw.com/2/ConfigParser/
 # https://linuxhint.com/python-configparser-example/
 # https://zetcode.com/python/configparser/
+# https://csatlas.com/python-script-path/
 
+config_parser = ConfigParser()
 
 # Get working directory
-workingdir = pathlib.Path.cwd()
-configfile = ['json2txt.conf','json2txt.ini']
-workingconfig = pathlib.Path(workingdir, configfile)
+#workingdir = pathlib.Path.cwd()
+workingdir = Path( __file__ ).parent.absolute()
+print('This is the script dir:', workingdir)
+configfile = 'conf.ini'
+workingconfig = pathlib.Path.joinpath(workingdir, configfile)
 read_files = config_parser.read(workingconfig)
 
+print('location of config:', workingconfig)
 config_parser.read(workingconfig)
-
-print(config_parser.get('filter', 'web_config_reddit'))
-
+print(config_parser.get('web_config_reddit', 'filter'))
+print(config_parser.get('global', 'mode'))
 
 
 
