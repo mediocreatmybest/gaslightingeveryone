d5b01ab13749165111ff4dd5841ac72900db5546
mediocreatmybest <80406625+mediocreatmybest@users.noreply.github.com>
2023-01-05 20:56:32 +0930

    Added texts2txts.py to help concat text files and remove dups and or filter txt files
    Added texts2txts.py to help concat text files, remove duplicate words, and or filter txt files to remove a few symbols. May be useful for word cloud or caption dumps of text files.

--- Scripts/func.py
+++ Scripts/func.py
@@ -34,3 +34,20 @@ def list2String_space_sep(list):
     seperator = " "
     #return string
     return (seperator.join(list))
+
+# Function to help cocatenate simple txt files
+import codecs
+import os
+
+def concatenate_files(root_dir, output_file):
+    """ Function to concatenate text files into a single file  """
+    with codecs.open(output_file, 'w', encoding='utf-8') as outfile:
+        for root, dirs, files in os.walk(root_dir):
+            for file in files:
+                if file.endswith('.txt'):
+                    file_path = os.path.join(root, file)
+                    with codecs.open(file_path, 'r', encoding='utf-8') as infile:
+                        # Concatenate the contents of the file to the output file
+                        outfile.write(infile.read())
+                        # Space seperator
+                        outfile.write(' ')
---
+++ Scripts/working/txts2txt.py
@@ -0,0 +1,72 @@
+import os
+import re
+import argparse
+from pathlib import Path
+from func import concatenate_files, list2String_space_sep
+
+# Create the arg parser
+parser = argparse.ArgumentParser()
+# Add an argument
+parser.add_argument('--inputdir', type=str, help='Directory with text files', metavar='c:\captions', required=True)
+parser.add_argument('--outputfile', type=str, help='Image directory to caption', metavar='c:\captions\allwords.txt', required=True)
+parser.add_argument('--dedup', action='store_true', help='Remove all duplicate words', required=False)
+parser.add_argument('--filter', action='store_true', help='Run a filter to remove single symbols', required=False)
+#parser.add_argument('--write', action='store_true', help='Allow writing or modifying files', required=False)
+# Add debug option to help disable save and prints out useful variables
+parser.add_argument('--debug', action='store_true', help='Disables Saving files, prints output locations', required=False)
+
+# Parse the argument
+cmd_args = parser.parse_args()
+
+con_path = Path(rf"{cmd_args.inputdir}")
+out_file = Path(rf"{cmd_args.outputfile}")
+out_file_dedup_base = (os.path.splitext(out_file)[0])
+out_file_dedup = Path(rf"{out_file_dedup_base}_dedup.txt")
+out_file_dedup_filtered = Path(rf"{out_file_dedup_base}_dedup_filtered.txt")
+
+# debug
+if cmd_args.debug is True:
+    print('Input dir: ', cmd_args.inputdir)
+    print('Output file: ', cmd_args.outputfile)
+    print('Dedup file is: ', out_file_dedup)
+    print('Filtered dedup file is: ', out_file_dedup_filtered)
+    print('remove duplicate words: ', cmd_args.dedup)
+    print('Filter text is: ', cmd_args.filter)
+    #print('Write files is: ', cmd_args.write)
+
+# Call and use the concatenate function to combine all text files
+concatenate_files(con_path, out_file)
+
+if cmd_args.dedup is True:
+
+    # Open the file and read its contents
+    with open(out_file, 'r', encoding='utf-8') as file:
+        contents = file.read()
+    # Split the contents into a list of words
+    words = contents.split()
+    # Convert the list to a set to remove duplicates
+    words_set = set(words)
+    sorted_words = sorted(words_set)
+
+    # Write the unique words back to the file
+    with open(out_file_dedup, 'w', encoding='utf-8') as file_dedup:
+        for word in sorted_words:
+            file_dedup.write(word + ' ')
+
+if cmd_args.filter is True and cmd_args.dedup is True:
+
+    # Convert the list to a string to filter with regex
+    filtered_words = list2String_space_sep(sorted_words)
+
+    # Create an exclusion list
+    exclusionList = [',', '\|', '–', '&quot;', '&nbsp;', '&amp', '&gt', '"', '”', ' ― ', '- ', ';']
+    exclusions = '|'.join(exclusionList)
+
+    # Remove all items in th exclusionList
+    filtered_words = re.sub(exclusions, '', filtered_words)
+    # Additional filtering
+    filtered_words = re.sub(r'\s+', ' ', filtered_words)
+
+    with open(out_file_dedup_filtered, 'w', encoding='utf-8') as file_filtered:
+        for word in filtered_words:
+            file_filtered.write(word)
