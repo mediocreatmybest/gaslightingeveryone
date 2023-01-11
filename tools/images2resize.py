2b1c64f3b6b72e9927ca15ed5f0e6046455046fc
mediocreatmybest <80406625+mediocreatmybest@users.noreply.github.com>
2023-01-12 01:31:09 +0930

    Update of images2resize.py - This took too long to get working...
    Update of images2resize.py - This took too long to get working...
    This should keep the same name but may get a different extension case when changing formats with jpg vs jpeg if the original is in upper case.

--- tools/images2resize.py
+++ tools/images2resize.py
@@ -7,17 +7,22 @@ from PIL import Image
 parser = argparse.ArgumentParser(description='Copy and resize all images in a directory recursively based on the shortest side while keeping the same name')
 
 # Set allowed images for directory scan (Sorry GIF)
-image_filter = ('jpeg','jpg','png','bmp')
-
-# TODO: Copy caption files
+image_filter = ('jpeg','jpg','png','bmp','webp')
 
 # Add the arguments
 parser.add_argument('--input-dir', metavar='c:\images', type=str, help='the input image directory path', required=True)
-parser.add_argument('--output-dir', metavar='c:\images-smaller', type=str, help='the image output directory path', required=True)
+parser.add_argument('--output-dir', metavar='c:\images_resize', type=str, help='the image output directory path', required=True)
 parser.add_argument('--size', metavar='576', type=int, help='desired size of the smallest side', required=True)
+parser.add_argument('--copy-format', action='store_true', default=False, help='Keeps the same file format from the input image')
+parser.add_argument('--format', metavar='jpg', type=str, help='Change the image format (jpeg, jpg, png, bmp, webp)')
 
 # Parse the arguments
 args = parser.parse_args()
+
+# Create error if copy-format is False and the format argument is None
+if args.copy_format is False and args.format is None:
+    raise Exception('Please select a format with --format or copy existing format with --copy-format')
+
 
 # Lets create the output directory if it doesn't exist
 if not os.path.exists(args.output_dir):
@@ -36,8 +41,11 @@ for root, dirs, files in os.walk(args.input_dir):
             # Get the width and height of the image
             width, height = image.size
             if args.size >= min(width, height):
-                # Better way to do this?
-                raise ValueError(f"The size you specified: {args.size} is WAY WAY too big. It should be smaller than the existing image: {file}")
+                # Better way to do this? It *should* still resize and keep toddling on
+                try:
+                    raise ValueError((f'The size you specified: {args.size} is WAY WAY too big. It should be smaller than the source image: {file}'))
+                except ValueError as err:
+                    print(err)
 
             # Determine the new size of the image
             if width < height:
@@ -48,11 +56,24 @@ for root, dirs, files in os.walk(args.input_dir):
             # Resize the image
             resized_image = image.resize(new_size)
 
-            # Save the resized image recursivily (hopefully)
-            resized_image.save(os.path.join(args.output_dir, file))
+            # Check for copy input format (I think this works)
+            if args.copy_format:
+                format = image.format
+            else:
+                format = args.format
+
+            # Save the resized image recursively while checking for jpeg vs jpg with its silly extension argument
+            if format.casefold() == 'jpg':
+                resized_image.save(os.path.join(args.output_dir, base_file + '.jpg'), 'jpeg')
+
+            if args.copy_format is False and format.casefold() != 'jpg':
+                resized_image.save(os.path.join(args.output_dir, base_file + '.'+format), format)
+
+            if args.copy_format:
+                resized_image.save(os.path.join(args.output_dir, file), format)
 
             # Check if the image file as a matching text file and copy to new directory
-            text_file = base_file + ".txt"
+            text_file = base_file + '.txt'
             if os.path.exists(os.path.join(root, text_file)):
                 # If it exists, copy it
                 shutil.copy2(os.path.join(root, text_file), os.path.join(args.output_dir, text_file))
