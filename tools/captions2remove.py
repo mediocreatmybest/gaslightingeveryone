da570adbd2e739f8ee843fe0b4306d682d707d94
mediocreatmybest <80406625+mediocreatmybest@users.noreply.github.com>
2023-01-14 19:20:13 +0930

    captions2remove.py updated
    captions2remove.py updated with text removal, text substitute, append, prepend.

--- tools/captions2remove.py
+++ tools/captions2remove.py
@@ -5,15 +5,40 @@ import argparse
 from pathlib import Path
 from tqdm import tqdm
 
+# Save function
+
+def save_file(file_path, data, mode='w', encoding='utf-8', debug=False):
+    """ Function to save a file, defaults to write mode """
+    if not debug:
+        with open(file_path, mode, encoding=encoding) as f:
+            f.write(data)
+        print(f'File saved to {file_path}')
+    else:
+        print('Debug mode, file not saved')
+
+
+def save_file_prepend(file_path, data, mode='r+', encoding='utf-8', debug=False):
+    """ Function to save with 'r+' at the start of a file seek(0) """
+    if not debug:
+        with open(file_path, mode, encoding=encoding) as f:
+            f.seek(0)
+            f.write(data)
+        print(f'File saved to {file_path}')
+    else:
+        print('Debug mode, file not saved')
+
+
 # Create the arg parser
-parser = argparse.ArgumentParser()
+parser = argparse.ArgumentParser(description='Allows you to selection root folders and recursivly modify plain text files')
 # Add an argument
 parser.add_argument('--captiondir', type=str, help='Directory with captions', metavar='c:\images', required=True)
-parser.add_argument('--caption-remove', type=str, help='Text to remove', metavar='"Monkey Magic is great!', required=True)
-parser.add_argument('--caption-replace', type=str, help='Image directory to caption', metavar='"Tripitaka is the best!"', required=True)
+parser.add_argument('--caption-find', type=str, help='Text to find and remove', metavar='"Monkey Magic is great!"', required=False)
+parser.add_argument('--caption-replace', type=str, help='Caption to change', metavar='"Tripitaka is the best!"', required=False)
+parser.add_argument('--caption-prepend', type=str, help='Insert text at the start of the caption', metavar='"Wow text at the start? say it aint so!"', required=False)
+parser.add_argument('--caption-append', type=str, help='Insert text at the end of the caption', metavar='"Wow text at the end? Amazing!"', required=False)
 parser.add_argument('--extension', type=str, help='File extension of caption files e.g. ".txt"', metavar='".txt"', default='.txt', required=False)
 # Debug
-parser.add_argument('--debug', action='store_true', help='Disables Saving files, prints output locations', required=False)
+parser.add_argument('--debug', action='store_true', default=False, help='Disables Saving files, prints output locations', required=False)
 
 # Parse the argument
 cmd_args = parser.parse_args()
@@ -32,38 +57,35 @@ for root, dirs, files in os.walk(captions_path):
                 print('######################################')
                 print('caption file: ', caption_file)
                 print('image caption base directory: ', caption_base_dir)
+                if cmd_args.caption_find:
+                    print(f'--caption-find argument: {cmd_args.caption_find}')
+                    print(f'--caption-replace argument: {cmd_args.caption_replace}')
+                if cmd_args.caption_prepend:
+                    print(f'--caption-prepend argument: {cmd_args.caption_prepend}')
+                if cmd_args.caption_append:
+                    print(f'--caption-append argument: {cmd_args.caption_append}')
                 print('######################################')
 
-            # Open the found caption file ending with .txt (TODO: update to allow string re.sub on any extension)
-            with open(caption_file, 'r', encoding='UTF-8') as caption_read:
-                data = caption_read.read()
-                # Push argument into new variable and run re.sub to remove specified text
-                replacement_str = re.sub(rf'{cmd_args.caption_remove}', rf'{cmd_args.caption_replace}', data, flags=re.I)
-                if cmd_args.debug is True:
-                    print('Data in txt file before re.sub: ')
-                    print('######################################')
-                    print(data)
-                    print('######################################')
-                    print('String to find: ', cmd_args.caption_remove)
-                    print('String to insert: ', cmd_args.caption_replace)
-                    print('Complete string is now: ')
-                    print('######################################')
-                    print(replacement_str)
-                    print('######################################')
+            # Open the found caption file with different arguments
 
-            # Check if debug is enabled to disable saving
-            if cmd_args.debug is True:
-                savefiles = False
-            else:
-                savefiles = True
+            if cmd_args.caption_find:
+                with open(caption_file, 'r', encoding='utf-8') as ff:
+                    data = ff.read()
+                    # If replacing strings we want to push the argument into new variable and run re.sub to remove specified text
+                    # Make omitting replace argument pass '' instead of None, this should allow simple removal without replacement arg
+                    if cmd_args.caption_replace is None:
+                        cmd_args.caption_replace = ''
+                    replacement_data = re.sub(rf'{cmd_args.caption_find}', rf'{cmd_args.caption_replace}', data, flags=re.I)
+                    save_file(file_path=caption_file, data=replacement_data, mode='w', debug=cmd_args.debug)
 
-            # Set write flag to overwrite
-            writeflag = 'w'
+            if cmd_args.caption_prepend:
+                # Time to save and move to the start of the file
+                with open(caption_file, 'r', encoding='utf-8') as fp:
+                    # Join the new data with the original data
+                    data = fp.read()
+                    prepend_data = cmd_args.caption_prepend + data
+                    save_file_prepend(file_path=caption_file, data=prepend_data, mode='r+', debug=cmd_args.debug)
 
-            if cmd_args.debug is True:
-                print('Set to overwrite existing files', writeflag)
-
-            # Create new file and overwrite if exists
-            if savefiles is True:
-                with open(caption_file, f'{writeflag}', encoding='UTF-8') as f:
-                    f.write(replacement_str)
+            if cmd_args.caption_append:
+                    appended_data = cmd_args.caption_append
+                    save_file(file_path=caption_file, data=appended_data, mode='a', debug=cmd_args.debug)
